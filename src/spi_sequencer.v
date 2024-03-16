88fe2996d03c1a8dcb533e506234ec25fe38f269
Kirill Maslov <kirjuha@gmail.com>
2024-03-17 01:27:23 +0200

    Corrected formatting

--- src/spi_sequencer.v
+++ src/spi_sequencer.v
@@ -8,18 +8,17 @@ module tt_um_maslovk_lcd_ctrl_top (clk, rst_n, ena, ui_in, uo_out, uio_in, uio_o
 //	output wire dc;
 //	output wire sck;
 //	output wire reset;
+	input  wire [7:0] ui_in;    // Dedicated inputs
+	output wire [7:0] uo_out;   // Dedicated outputs
+	input  wire [7:0] uio_in;   // IOs: Input path
+	output wire [7:0] uio_out;  // IOs: Output path
+	output wire [7:0] uio_oe;   // IOs: Enable path (active high: 0=input, 1=output)
 
 	wire cs;
 	wire dc;
 	wire reset;
 	wire sck;
 	wire mosi;
-
-	input  wire [7:0] ui_in,    // Dedicated inputs
-    	output wire [7:0] uo_out,   // Dedicated outputs
-    	input  wire [7:0] uio_in,   // IOs: Input path
-    	output wire [7:0] uio_out,  // IOs: Output path
-    	output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
 
 	assign uo_out[0] = cs;
 	assign uo_out[1] = dc;
